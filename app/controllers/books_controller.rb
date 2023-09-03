a68d7961bc647429ffa1ac0409feef8044ffd70f
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-09-03 19:20:39 +0000

    応用課題２途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,45 +2,60 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    render :edit
+    else
+  redirect_to books_path
+    end
+  end
+
+  def new
+    @book = Book.new
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+  def destroy
+    book = Book.find(params[:id])
+    book.destoy
+    redirect_to '/books'
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+
   end
 
   def index
     @users = User.all
-    @book = Book.new
+    @user = current_user
   end
 
   def edit
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def ensure_correct_user
